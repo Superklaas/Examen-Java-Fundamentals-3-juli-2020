1a782ae056d8d2d05642921c3ea8a097648db4b6
Superklaas <67093947+Superklaas@users.noreply.github.com>
2020-07-03 14:12:04 +0200

    Add files via upload

---
+++ randomobject/RandomObject.java
@@ -0,0 +1,27 @@
+package be.vdab.randomobject;
+
+import java.util.Random;
+
+public class RandomObject {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int[] array = new int[25];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(1000);
+        }
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+            if(array[i]<250) {
+                System.out.println("PIEF");
+            }
+            else if(array[i]<500) {
+                System.out.println("POEF");
+            }
+            else {
+                System.out.println("PAF");
+            }
+        }
+
+    }
+
+}
