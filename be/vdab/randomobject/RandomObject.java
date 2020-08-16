35b5d8f525290d9986f36f0c0794ec9d6655904c
Elias Khan <khan.elias@gmail.com>
2020-08-17 01:24:25 +0200

    randomObject refactorings. Good job!

--- be/vdab/randomobject/RandomObject.java
+++ be/vdab/randomobject/RandomObject.java
@@ -9,15 +9,13 @@ public class RandomObject {
         for (int i = 0; i < array.length; i++) {
             array[i] = random.nextInt(1000);
         }
-        for (int i = 0; i < array.length; i++) {
-            System.out.println(array[i]);
-            if(array[i]<250) {
+        for (int j : array) {
+            System.out.println(j);
+            if (j < 250) {
                 System.out.println("PIEF");
-            }
-            else if(array[i]<500) {
+            } else if (j < 500) {
                 System.out.println("POEF");
-            }
-            else {
+            } else {
                 System.out.println("PAF");
             }
         }
